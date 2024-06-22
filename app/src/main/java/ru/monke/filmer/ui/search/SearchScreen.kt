31f9bcd87309aa0a2f1d82c4706425fd57500fbf
Igor Streshnev <igor.streshnev2019@gmail.com>
2024-06-22 19:01:35 +0300

    category selection

--- app/src/main/java/ru/monke/filmer/ui/search/SearchScreen.kt
+++ app/src/main/java/ru/monke/filmer/ui/search/SearchScreen.kt
@@ -1,9 +1,17 @@
 package ru.monke.filmer.ui.search
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -13,7 +21,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ru.monke.filmer.R
@@ -23,6 +33,8 @@ import ru.monke.filmer.ui.common.ShowItem
 import ru.monke.filmer.ui.common.ShowsList
 import ru.monke.filmer.ui.mockedList
 import ru.monke.filmer.ui.theme.FilmerTheme
+
+const val ALL_CATEGORIES = "All"
 
 @Composable
 fun SearchScreen(
@@ -49,12 +61,71 @@ fun SearchScreen(
                     .fillMaxWidth()
                     .padding(top = 16.dp)
             )
+            CategoryChipGroup(
+                categoriesList = listOf(ALL_CATEGORIES, "Action", "Slasher", "Smash someone balls"),
+                modifier = Modifier.padding(top = 16.dp)
+            )
             ShowItem(
                 modifier = Modifier.padding(top = 24.dp),
                 show = todayShow)
             ShowsList(mockedList, stringResource(id = R.string.recommended_for_you))
         }
 
+    }
+}
+
+@Composable
+fun CategoryChipGroup(
+    categoriesList: List<String>,
+    modifier: Modifier = Modifier
+) {
+    var selectedCategory by remember {
+        mutableStateOf(ALL_CATEGORIES)
+    }
+    LazyRow(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(categoriesList) { category ->
+            if (category == selectedCategory) {
+                SelectedCategoryChip(title = category)
+            } else {
+                CategoryChip(
+                    modifier = Modifier
+                        .clickable { selectedCategory = category },
+                    title = category
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun CategoryChip(
+    modifier: Modifier = Modifier,
+    title: String
+) {
+    Text(
+        text = title,
+        modifier = modifier.padding(horizontal = 12.dp, vertical = 8.dp)
+    )
+}
+
+@Composable
+fun SelectedCategoryChip(
+    modifier: Modifier = Modifier,
+    title: String
+) {
+    Surface(
+        modifier = modifier,
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Text(
+            text = title,
+            modifier = Modifier
+                .padding(horizontal = 18.dp, vertical = 8.dp),
+            textAlign = TextAlign.Center
+        )
     }
 }
 
