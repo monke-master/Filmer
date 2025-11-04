77696857bf9b10ae573177e4d9dfc4334f455a7b
Igor Streshnev <igor.streshnev2019@gmail.com>
2025-11-04 16:15:55 +0300

    ci/cd: gihub actions

--- app/src/test/java/ru/monke/filmer/ExampleUnitTest.kt
+++ app/src/test/java/ru/monke/filmer/ExampleUnitTest.kt
@@ -14,4 +14,10 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun addition_isCorrectWithPrinting() {
+        println("Start tests")
+        assertEquals(4, 2 + 2)
+    }
 }
